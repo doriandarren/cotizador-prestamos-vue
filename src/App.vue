b98528fba152c06b97beb96be9dfe794b2c5933d
Dorian Gonzalez <doriandarren1@gmail.com>
2024-08-04 22:07:54 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+  import Header from './components/Header.vue';
+</script>
+
+<template>
+  <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
+    <Header />
+  </div>
+</template>
